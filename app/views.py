943159faf7512ef16e9d769e545c5e00e7a5f015
4faramita <4faramita@gmail.com>
2017-04-17 12:34:44 +0800

    Show all todos from Database

--- app/views.py
+++ app/views.py
@@ -1,7 +1,10 @@
-from app import app
 from flask import render_template
+
+from app import app
+from app.models import ToDo
 
 
 @app.route('/')
 def index():
-    return render_template('index.html', text='Hello')
+    todos = ToDo.objects.all()
+    return render_template('index.html', todos=todos)
