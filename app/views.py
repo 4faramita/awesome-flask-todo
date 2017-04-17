14458524f70c8ce944ede0cae9807e61d36af46d
4faramita <4faramita@gmail.com>
2017-04-17 12:51:32 +0800

    Add To-Do in browser.

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, request
 
 from app import app
 from app.models import ToDo
@@ -6,5 +6,14 @@ from app.models import ToDo
 
 @app.route('/')
 def index():
+    todos = ToDo.objects.all()
+    return render_template('index.html', todos=todos)
+
+@app.route('/add', methods=['post',])
+def add():
+    content = request.form.get('content')
+    todo = ToDo(content=content)
+    todo.save()
+    
     todos = ToDo.objects.all()
     return render_template('index.html', todos=todos)
