8210cd48453b8bdd5873c534817f06242a3bfe57
4faramita <4faramita@gmail.com>
2017-04-17 13:04:57 +0800

    Do and undo a task in browser

--- app/views.py
+++ app/views.py
@@ -9,6 +9,7 @@ def index():
     todos = ToDo.objects.all()
     return render_template('index.html', todos=todos)
 
+
 @app.route('/add', methods=['post',])
 def add():
     content = request.form.get('content')
@@ -17,3 +18,23 @@ def add():
     
     todos = ToDo.objects.all()
     return render_template('index.html', todos=todos)
+
+
+@app.route('/do/<string:todo_id>')
+def do(todo_id):
+    todo = ToDo.objects.get_or_404(id=todo_id)
+    todo.status = 1
+    todo.save()
+
+    todos = ToDo.objects.all()
+    return render_template("index.html", todos=todos)
+
+
+@app.route('/undo/<string:todo_id>')
+def undo(todo_id):
+    todo = ToDo.objects.get_or_404(id=todo_id)
+    todo.status = 0
+    todo.save()
+    
+    todos = ToDo.objects.all()
+    return render_template("index.html", todos=todos)
